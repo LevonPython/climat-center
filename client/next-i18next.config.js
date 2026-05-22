1feaa59a4022a944db9ef31c7ba8f9c929e34c6f
LevonPython <levon.python@gmail.com>
2026-05-23 00:36:45 +0400

    fix(i18n): update next-i18next configuration to bundle locale resources and adjust localePath for SSR compatibility

--- client/next-i18next.config.js
+++ client/next-i18next.config.js
@@ -1,10 +1,22 @@
+const path = require('path');
+
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: 'ru',
     locales: ['ru', 'en', 'am']
   },
-  localePath: './public/locales',
+  // Bundle locale JSON so ISR/SSR on Vercel can read translations without filesystem access to public/.
+  resources: {
+    ru: { common: require('./public/locales/ru/common.json') },
+    en: { common: require('./public/locales/en/common.json') },
+    am: { common: require('./public/locales/am/common.json') }
+  },
+  ns: ['common'],
+  defaultNS: 'common',
+  localePath:
+    typeof window === 'undefined'
+      ? path.resolve(process.cwd(), 'public/locales')
+      : '/locales',
   returnObjects: true
 };
-
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import '../styles/globals.css';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextI18nextConfig = require('../next-i18next.config');
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default appWithTranslation(App);
-
+export default appWithTranslation(App, {
+  ...nextI18nextConfig,
+  localePath: '/locales'
+});
